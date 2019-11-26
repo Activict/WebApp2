81f3a83c953f28ac66f25c9a687a8820826f7b79
MaxPavlov <activict1@gmail.com>
2019-11-26 16:24:54 +0300

    add Cart db, model, controller

---
+++ WebApp/Models/Data/ShopCartDB.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Data
+{
+    [Table("ShopCart")]
+    public class ShopCartDB
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int ProductId { get; set; }
+        public int AmountProduct { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UserModel user { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual ShopProductDB Product { get; set; }
+    }
+}
---
+++ WebApp/Models/Shop/ShopCartVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApp.Models.Data;
+
+namespace WebApp.Models.Shop
+{
+    public class ShopCartVM
+    {
+        public ShopCartVM() { }
+        public ShopCartVM(ShopCartDB model)
+        {
+            Id = model.Id;
+            UserId = model.UserId;
+            ProductId = model.ProductId;
+            AmountProduct = model.AmountProduct;
+        }
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int AmountProduct { get; set; }
+    }
+}
